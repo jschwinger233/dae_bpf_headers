3372fbdbc2277d691f059c5f4979e2d9a22809f9
Gray Liang <gray.liang@isovalent.com>
2024-02-04 17:54:28 +0800

    Use vmlinux-x64.h for unknown arch

--- vmlinux.h
+++ vmlinux.h
@@ -3,5 +3,10 @@
 #elif defined(__TARGET_ARCH_arm64)
 #include "vmlinux-arm64.h"
 #else
-#error "Unknown architecture"
+/*
+ * For other architectures, we don't have a vmlinux.h file. But the normal dae
+ * bpf program doesn't need it. So we just include the x86 vmlinux.h file to
+ * make the build pass.
+ */
+#include "vmlinux-x86.h"
 #endif
